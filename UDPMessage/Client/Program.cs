ef28f09752f716e2f301b2c22c68081558288c4f
Everson Turossi <eversont@gmail.com>
2024-09-06 11:28:04 -0300

    Exemplo de envio de mensagem UDP

---
+++ UDPMessage/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPMessageClient
+{
+    static void Main()
+    {
+        var port = 45123;
+        // Endereço IP do servidor (substitua pelo IP do servidor real)
+        var serverIP = "127.0.0.1";
+        string udpMessage = "UDP Message Example";
+
+        // Converte a mensagem em bytes
+        byte[] sendBytes = Encoding.ASCII.GetBytes(udpMessage);
+
+        // Cria um socket UDP
+        UdpClient udpClient = new UdpClient();
+
+        try
+        {
+            // Define o destino para o servidor
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIP), port);
+
+            // Envia a mensagem LLDP para o servidor
+            udpClient.Send(sendBytes, sendBytes.Length, serverEP);
+
+            Console.WriteLine($"UDP message sent to {serverEP.Address}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            udpClient.Close();
+        }
+    }
+}
---
+++ UDPMessage/Listener/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPMessageListener
+{
+    static void Main()
+    {
+        int port = 45123;
+        // Cria um socket UDP
+        UdpClient udpClient = new UdpClient(port);
+
+        try
+        {
+            Console.WriteLine("Listening for UDP messages...");
+
+            // Aguarda por pacotes UDP
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, port);
+            while (true)
+            {
+                byte[] receiveBytes = udpClient.Receive(ref remoteEP);
+                string lldpMessage = Encoding.ASCII.GetString(receiveBytes);
+
+                Console.WriteLine($"Received UDP message from {remoteEP.Address}: {lldpMessage}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            udpClient.Close();
+        }
+    }
+}
